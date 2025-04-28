7ae1df5edf7da25472e1dfc780aa83fca14271ea
Jayanthi Mudhangila <132087882+Jayanthi2723@users.noreply.github.com>
2025-04-28 16:44:25 +0530

    Add files via upload

---
+++ EventManagementSystems/utils/footer.php
@@ -0,0 +1,39 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>S</strong><span class = "small footerSubtext">ri Venkatesa</span>
+                        <strong>P</strong><span class = "small footerSubtext">erumal College of</span>
+                        <strong>E</strong><span class = "small footerSubtext">ngineering and</span>
+                        <strong>T</strong><span class = "small footerSubtext">echnology</span>
+                    </p>
+
+                    <p class = "footerSubtext2">
+                        RVS Nagar, K.N. Road, Cherlopalle,
+                        Andra Pradesh, 517583.
+                        &copy; MJ Events Venue & Catering, 2022.
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                    DL824 Multimedia Design/Web Engineering<br>
+                    Creative Computing 2021-2022.<br>
+                    CSE, N00145647
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                        <img src = "images/facebook.png">
+                        <img src = "images/twitter.png">
+                        <img src = "images/googleplus.png">
+                        <img src = "images/youtube.png">
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
